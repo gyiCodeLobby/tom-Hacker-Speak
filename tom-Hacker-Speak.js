93281cedb4e8de0864de3a2c5d6db00961835c66
Thomas <gotenmelo@gmail.com>
2015-08-17 21:46:08 -0400

    the challenge on chapter 4
    this is the challenge on chapter 4 cant believe i had lost the other one

---
+++ tom-Hacker-Speak.js
@@ -0,0 +1,8 @@
+var hacker = function(input,output){
+  console.log("Javascrpit is awesome","4 += a");
+  var a = "Javascript is awesome";
+  var b = "4 += a";
+};
+console.log(hacker);
+var input = "Javascript is awesome";
+var output ="";
